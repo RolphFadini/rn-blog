b3ef956c6ea28584522b287988e0593f4d82dfc2
Rolph Fadini <rolphfadini@hotmail.com>
2025-06-20 14:42:54 -0400

    feat: move add post action to navigation header in IndexScreen

--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -4,21 +4,16 @@ import {
 	Text,
 	StyleSheet,
 	FlatList,
-	Button,
 	TouchableOpacity,
 } from "react-native";
 import { Context } from "../context/BlogContext";
 import { Feather } from "@expo/vector-icons";
 
 const IndexScreen = ({ navigation }) => {
-	const { state, addBlogPost, deleteBlogPost } = useContext(Context);
+	const { state, deleteBlogPost } = useContext(Context);
 
 	return (
 		<View>
-			<Button
-				title="Add Post"
-				onPress={addBlogPost}
-			/>
 			<FlatList
 				data={state}
 				keyExtractor={(blogPost) => blogPost.title}
@@ -50,6 +45,20 @@ const IndexScreen = ({ navigation }) => {
 	);
 };
 
+IndexScreen.navigationOptions = ({ navigation }) => {
+	return {
+		headerRight: () => (
+			<TouchableOpacity onPress={() => navigation.navigate("Create")}>
+				<Feather
+					name="plus"
+					size={30}
+					style={styles.newPost}
+				/>
+			</TouchableOpacity>
+		),
+	};
+};
+
 const styles = StyleSheet.create({
 	row: {
 		flexDirection: "row",
@@ -64,6 +73,9 @@ const styles = StyleSheet.create({
 	},
 	icon: {
 		fontSize: 24,
+	},
+	newPost: {
+		marginRight: 20,
 	},
 });
 
